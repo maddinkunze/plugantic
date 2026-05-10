9a1d1c7bf401176e2dd27f338ff1fdc9e6c6c04b
Martin Kunze <martin@martinkunze.com>
2026-05-10 15:25:14 +0200

    chore: expose all new relevant types via the modules init file

--- src/plugantic/__init__.py
+++ src/plugantic/__init__.py
@@ -1,3 +1,4 @@
 """Plugantic: Simplify extendable composition with Pydantic."""
 from ._consts import version as __version__
-from .plugin import PluginModel, Field
+from .plugin import PluginModel, PluganticConfigDict as PluginConfig, Field
+from .plugin import PluginAdapter, PluginUnion, PluginIntersection
